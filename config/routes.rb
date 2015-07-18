56b3b531e61217131e35f0c30eaaf6f576f04387
nhs3108 <nhs3108@gmail.com>
2015-07-18 11:04:54 +0700

    Show all courses with their information

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root "dashboards#index"
     resources :subjects, except: [:show, :destroy]
+    resources :courses, only: [:index]
   end
 
   devise_for :users, path: "", path_names: {

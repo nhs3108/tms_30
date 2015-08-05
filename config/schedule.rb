997ec9328e0fc43e57ede481d7302fae05b48a89
nhs3108 <nhs3108@gmail.com>
2015-08-05 17:11:29 +0700

    Update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,12 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require "active_support/time"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
-# Learn more: http://github.com/javan/whenever
-every :month, at: "end of the month at 6:00am" do
-  runner "Course.send_all_courses_info"
+(2015..2025).each do |year|
+  (1..12).each do |month|
+    every "0 6 " + Time.days_in_month(month, year).to_s + " #{month} #{year}" do
+      runner "Course.send_all_courses_info"
+    end
+  end
 end
 
 every :day, at: "6:00am" do
